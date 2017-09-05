fde83ea154e8cbafa5ddd7ef140b1ccc93e683be
snehacv <sneha.cv@gmail.com>
2017-09-05 17:31:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one',function(req,res){
+    res.send("Articleone Will be served soon");
+}
+)
+app.get('article-one',function(req,res){
+    res.send("Articletwo Will be served soon");
+}
+)
+app.get('article-one',function(req,res){
+    res.send("Articlethree Will be served soon");
+}
+)
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
